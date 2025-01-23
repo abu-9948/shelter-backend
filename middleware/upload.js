05b6ab602db33561ab9d860bbf4703e81108e17e
abu-9948“ <abubakarsiddiqueshaik@ABUs-MacBook-Air.local>
2025-01-23 15:10:20 +0530

    test

--- middleware/upload.js
+++ middleware/upload.js
@@ -3,10 +3,8 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
